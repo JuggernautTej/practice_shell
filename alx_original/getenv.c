2aee170872d63489a018cdc52228c4ec5a6dda03
VectorGits <davidoluremi0@gmail.com>
2023-12-12 15:04:30 +0100

    Fixed getenv error

--- alx_original/getenv.c
+++ alx_original/getenv.c
@@ -7,16 +7,22 @@
  */
 char *_getenv(char *name)
 {
-char **my_environ = environ;
-int x;
-char *tkn;
-for (x = 0; my_environ[x] != NULL; x++)
-{
-tkn = strtok(my_environ[x], "=");
-if (_strcmp(tkn, name) == 0)
-{
-return (strtok(NULL, "="));
-}
-}
-return (NULL);
+    char **my_environ = environ;
+    int x;
+    char *tkn;
+    char *env_copy;
+
+    for (x = 0; my_environ[x] != NULL; x++)
+    {
+        env_copy = _strdup(my_environ[x]);  // Make a copy of the environment variable
+        tkn = strtok(env_copy, "=");
+        if (_strcmp(tkn, name) == 0)
+        {
+            char *value = _strdup(strtok(NULL, "="));  // Copy the value
+            free(env_copy);  // Free the copy of the environment variable
+            return value;
+        }
+        free(env_copy);  // Free the copy of the environment variable
+    }
+    return (NULL);
 }
